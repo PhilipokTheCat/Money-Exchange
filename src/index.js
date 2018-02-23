55a92f21f62316374174ba88633d8cc63ff09b96
unknown <philipok4596@gmail.com>
2018-02-24 02:26:36 +0300

    money-exchange/feature: implement money exchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let CoinsCostArray = [["H", 0], ["Q", 0], ["D", 0], ["N", 0], ["P", 0]];
+    let result = {};
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    while (currency > 0){
+        if (currency >= 50) {currency -= 50; CoinsCostArray[0][1]++;}
+        else if (currency >= 25) {currency -= 25; CoinsCostArray[1][1]++;}
+        else if (currency >= 10) {currency -= 10; CoinsCostArray[2][1]++;}
+        else if (currency >= 5) {currency -= 5; CoinsCostArray[3][1]++;}
+        else {currency -= 1; CoinsCostArray[4][1]++;}
+    }
+    CoinsCostArray.forEach((elem)=>{
+        if (elem[1] !== 0) result[elem[0]] = elem[1];   
+    })
+    return result;
 }
